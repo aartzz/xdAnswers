cf3025afbb7d48477752b8ccf459c5cd6c6b0204
aartzz <migor1103@gmail.com>
2025-05-27 08:54:48 +0300

    #5 should be fixed

---
+++ .web-ext-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    sourceDir: './',
+    artifactsDir: './build',
+    ignoreFiles: [
+      '.git',
+      '.github',
+      'node_modules',
+      'package.json',
+      'package-lock.json',
+      '.web-ext-config.js',
+      'README.md',
+      'chrome_package',
+      'chrome-extension.zip'
+    ],
+  };
